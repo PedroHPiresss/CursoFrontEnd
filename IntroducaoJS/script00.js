c634dcbefd64d1d6546abfd8c6b95ec13c457271
PedroHPiresss <phpires2008@gmail.com>
2025-02-06 11:27:22 -0300

    06.02.25 - Introdução JS

---
+++ IntroducaoJS/script00.js
@@ -0,0 +1,32 @@
+const texto = "Olá Mundo!";
+var boasVindas = "Bem-Vindo, Pedro"
+boasVindas = "Boas Vindas, mlkada"
+
+console.log(texto);
+console.log(boasVindas)
+
+//Tipos de Dados
+
+let string = "Variável de Texto"; //Sempre entre Aspas duplas ou simples
+let numero1 = 42; //Número Inteiro
+let numero2 = 10.65; //Número Decimal
+let numero3 = "2"; // Número em Texto
+let boolean = true;
+let boolean2 = false;
+let boolean3 = "true"; //Texto
+let undefined;
+let nula = null;
+
+//let, var, const
+let resultado = true; //atribuição
+resultado = false; // reatribui
+//let resultado = true; - proibido reecrever a variável
+
+var nota = 10; //Atribuição
+nota = 9; //Reatribuição
+var nota = 8; //Reescrita
+
+const fixo = 42;
+//fixo = 43; Erro - Não posso reatribuir
+//const fixo = 41; Erro - Não posso reescrever
+
