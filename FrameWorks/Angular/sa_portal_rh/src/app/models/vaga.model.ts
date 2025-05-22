4da9fa4242972b70d316a1159f844c7624720bef
PedroHPiresss <phpires2008@gmail.com>
2025-05-22 11:28:49 -0300

    22.05.2025

---
+++ FrameWorks/Angular/sa_portal_rh/src/app/models/vaga.model.ts
@@ -0,0 +1,58 @@
+export class Vaga {
+  //atributos
+  //    id: number = 0;
+  //  nome: string = "";
+  //  foto: string = "";
+  //  descricao: string = "";
+  //  salario: number = 0;
+
+  //    constructor(id:number, nome:string, foto:string, descricao:string, salario:number){
+  //      this.id = id;
+  //      this.nome = nome;
+  //      this.foto = foto;
+  //      this.descricao = descricao;
+  //      this.salario = salario;
+  //  }
+
+  // ^ Forma clássica de criação de classes
+
+  constructor(
+    public id: number,
+    public nome: string,
+    public foto: string,
+    public descricao: string,
+    public salario: number
+  ) {}
+
+  // ^ Forma encurtada de criação de classes
+
+  //getters e setters -> quando os atributos são privados (encapsulamento)
+  //    getId(): number{
+  //      return this.id;
+  //}
+  //setId(id:number): void{
+  //  this.id = id;
+  // }
+
+  //toMap Obj para a API
+  toMap(): {[key: string]: any} {
+    return{
+      id: this.id,
+      nome: this.nome,
+      foto: this.foto,
+      descricao: this.descricao,
+      salario: this.salario
+    }
+  }
+
+  //fromMap API para Obj
+  fromMap(map:any):Vaga{
+    return new Vaga(
+      map.id,
+      map.nome,
+      map.foto,
+      map.descricao,
+      map.salario
+    )
+  }
+}
---
+++ FrameWorks/Angular/sa_portal_rh/src/app/services/vagas.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Vaga } from '../models/vaga.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VagasService {
+  //atributo
+  private apiUrl = "http://localhost:3013/vagas"; //Caminho da API
+
+  constructor(private http: HttpClient) { }
+
+  //Método de conexão com a API
+  
+  //get - read
+  getVagas(): Observable<Vaga[]> { // Responsável por traduzir as informações da API para Model
+    return this.http.get<Vaga[]>(this.apiUrl); // Faz a requisição para a API - Endereço da conexão e retorno da informação
+    
+  }
+
+  //post - create
+  cadastrarVaga(vaga: Vaga): Observable<Vaga[]> { // Responsável por traduzir as informações da API para Model
+    return this.http.post<Vaga[]>(this.apiUrl, vaga); // Faz a requisição para a API - Endereço da conexão e retorno da informação
+  }
+
+  //put - update
+  atualizarVaga(id: any, vaga:Vaga): Observable<Vaga[]> { // Responsável por traduzir as informações da API para Model
+    const urlAtualizado = `${this.apiUrl}/${id}`; // Faz a requisição para a API - Endereço da conexão e retorno da informação
+    return this.http.put<Vaga[]>(urlAtualizado, vaga);
+  }
+
+  //delete - delete
+  removerVaga(id:any): Observable<Vaga[]>{
+    const urlDeletar = `${this.apiUrl}/${id}`;
+    return this.http.delete<Vaga[]>(urlDeletar);
+  }
+}
